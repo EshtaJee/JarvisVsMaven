733befb81385cb07ccbdb7333aaf1bd5936b341f
EshtaJee <Logrus-10@yandex.ru>
2022-08-24 10:12:26 +0300

    BonusAutoFix

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -6,15 +6,15 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
+
         long amount = 1000;
         boolean registered = true;
         long expected = 30;
 
-        // вызываем целевой метод:
+
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+
         assertEquals(expected, actual);
     }
 
@@ -22,45 +22,45 @@ public class BonusServiceTest {
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
+
         long amount = 1000000;
         boolean registered = true;
         long expected = 500;
 
-        // вызываем целевой метод:
+
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+
         assertEquals(expected, actual);
     }
     @org.junit.jupiter.api.Test
     void shouldCalculateForUnRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
+
         long amount = 1000;
         boolean registered = false;
         long expected = 10;
 
-        // вызываем целевой метод:
+
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+
         assertEquals(expected, actual);
     }
     @org.junit.jupiter.api.Test
     void shouldCalculateForUnRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
+
         long amount = 1000000;
         boolean registered = false;
         long expected = 500;
 
-        // вызываем целевой метод:
+
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+
         assertEquals(expected, actual);
     }
 
